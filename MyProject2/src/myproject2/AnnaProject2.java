fb3021cc21beb71aaa84669ad9b76b13f9bda76c
annasanya <annasanya@student.uns.ac.id>
2022-08-30 12:52:52 +0700

    Pertemuan kedua
    Anna Sanya Khazanah

---
+++ MyProject2/src/myproject2/AnnaProject2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author HP
+ */
+public class AnnaProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("SSS  ");
+        System.out.print(" AA  ");
+        System.out.println("N  N ");
+        System.out.print("S  S ");
+        System.out.print("A  A ");
+        System.out.println("NN N");
+        System.out.print(" S   ");
+        System.out.print("A  A ");
+        System.out.println("NN N");
+        System.out.print("   S ");
+        System.out.print("AAAA ");
+        System.out.println("N NN ");
+        System.out.print("S  S ");
+        System.out.print("A  A ");
+        System.out.println("N NN ");
+        System.out.print(" SSS ");
+        System.out.print("A  A ");
+        System.out.print("N  N");
+    }
+    
+}
